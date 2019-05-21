c43ab46536b7ba178297262f4693e86a9d1fd2fb
Marko Stefanovic <marko35stefanovic@yahoo.com>
2019-05-21 17:08:25 +0200

    Initial models created

--- site/psi19/quiz/admin.py
+++ site/psi19/quiz/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Question)
+admin.site.register(Category_movies)
+admin.site.register(Category_geography)
+admin.site.register(Category_music)
+admin.site.register(Category_history)
+admin.site.register(Category_sports)
+
+admin.site.register(Game)
+admin.site.register(Senior_user)
+admin.site.register(Friendship)
+admin.site.register(User_profile)
