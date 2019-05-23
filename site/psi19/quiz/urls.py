390b6ff914226d2c0f76c1dad42c7bde8fbd28c9
fifiman <bauk.nenad@gmail.com>
2019-05-23 09:54:06 +0200

    Added base and fix css.

--- site/psi19/quiz/urls.py
+++ site/psi19/quiz/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'quiz'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home',        views.home,         name='home'),
+    path('signup',      views.signup,       name='signup')
 ]
